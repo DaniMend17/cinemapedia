0bdfaebd60556b08e5c746ffd0d7f8b88f924951
DaniMend17 <lexterlutor12@gmail.com>
2025-02-23 18:25:47 -0600

    Nombre de peliculas agregado

--- lib/presentation/screens/movies/movies_slideshow.dart
+++ lib/presentation/screens/movies/movies_slideshow.dart
@@ -8,16 +8,27 @@ class MoviesSlideshow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
     return SizedBox(
       height: 210,
       width: double.infinity,
       child: Swiper(
         viewportFraction: 0.8,
         scale: 0.9,
+        pagination: SwiperPagination(
+          margin: EdgeInsets.only(top: 0),
+          builder: DotSwiperPaginationBuilder(
+            activeColor: colors.primary,
+            color: colors.secondary
+          )
+        ),
         autoplay: true,
         itemCount: movies.length,
         itemBuilder: (context, index) {
-          return _Slide(movies[index]);
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 30),
+            child: _Slide(movies[index]),
+          );
         },
       ),
     );
@@ -30,19 +41,39 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Image.network(
-        movie.backdropPath,
-        fit: BoxFit.cover,
-        loadingBuilder: (context, child, loadingProgress) {
-          if (loadingProgress != null) {
-            return const DecoratedBox(
-                decoration: BoxDecoration(color: Colors.black12)
-            );
-          }
-          return child;
-        },
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20))
+      ),
+      clipBehavior: Clip.hardEdge,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress != null) {
+                return const DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.black12)
+                );
+              }
+              return child;
+            },
+          ),
+          Positioned(
+            bottom: 10,
+            left: 15,
+            child: Text(
+              movie.title,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.bold
+              )
+            ),
+          )
+        ],
       ),
     );
   }
